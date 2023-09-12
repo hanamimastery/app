3d6979aa6e3c4610d66590c04d4364522b98a2ca
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-09-12 23:06:26 +0200

    Fix frontmatter parser to load YAML correctly

--- config/providers/parsers.rb
+++ config/providers/parsers.rb
@@ -4,7 +4,8 @@ Hanami.app.register_provider :parsers, namespace: true do
   prepare do
     require 'front_matter_parser'
 
-    parser = FrontMatterParser::Parser.new(:md)
+    loader = FrontMatterParser::Loader::Yaml.new(allowlist_classes: [Date])
+    parser = FrontMatterParser::Parser.new(:md, loader: loader)
     register 'front_matter', parser
   end
 end
