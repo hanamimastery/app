2da2546a963a375e46671e96ff4002647dac2384
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-09-21 07:55:42 +0200

    Add better rom-factory configuration

--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -2,6 +2,23 @@
 
 require 'rom-factory'
 
+
 module Test
   Factory = ROM::Factory.configure { |config| config.rom = Test::Database.rom }
+  module FactoryMethods
+    def create(factory_name, **attrs)
+      Factory[factory_name, **attrs]
+    end
+
+    def build(factory_name, attrs)
+      Factory.structs[factory_name, **attrs]
+    end
+  end
+end
+
+using Refinements::Pathnames
+Pathname.require_tree SPEC_ROOT, 'support/factories/**/*.rb'
+
+RSpec.configure do |config|
+  config.include Test::FactoryMethods
 end
