f56dd71863df76b310f96889bc4e7b1497d42383
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-09-21 07:58:00 +0200

    Rename fetch_draft_episodes interactor spec

--- spec/slices/main/interactors/fetch_draft_episodes_spec.rb
+++ spec/slices/main/interactors/fetch_draft_episodes_spec.rb
@@ -2,6 +2,7 @@
 
 RSpec.describe Main::Interactors::FetchDraftEpisodes do
   let(:subject) { described_class.new() }
+
   describe  ".call" do
     it 'imports all the details from the new episodes' do
 
