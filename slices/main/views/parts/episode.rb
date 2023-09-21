4709a1ac2ad18e654dcd0f9c9dcc4cf754aec953
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-09-21 07:55:02 +0200

    Add part to show status as icon

---
+++ slices/main/views/parts/episode.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Main
+  module Views
+    module Parts
+      class Episode < Part
+        def status_icon
+          case value.status
+          when Main::Entities::Episode::STATUSES[:draft]
+            "✍️"
+          when Main::Entities::Episode::STATUSES[:scheduled]
+            "⌛"
+          when Main::Entities::Episode::STATUSES[:published]
+            "🌎"
+          end
+        end
+      end
+    end
+  end
+end
