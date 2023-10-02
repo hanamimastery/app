0ae37fe6e53a30100f89c29426bc69838fcf29ce
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-10-02 12:51:48 +0200

    Remove tests for legacy route

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Root', type: :request do
     get '/'
 
     # Find me in `config/routes.rb`
-    expect(last_response).to be_successful
-    expect(last_response.body).to include('<h1>Welcome to HanamiMastery!</h1>')
+    # expect(last_response).to be_successful
+    # expect(last_response.body).to include('<h1>Welcome to HanamiMastery!</h1>')
   end
 end
