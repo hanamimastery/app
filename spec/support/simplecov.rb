b59cee0ee91b992729a167f831c0f7b53c83111e
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-11-12 18:48:40 +0100

    BUG: Disable simplecov for now due to segmentation fault error
    When fatures tests are called with simplecov, it raises segmentation fault

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-require 'simplecov'
+# Disabled simplecov as it causes segmentation fault when called with "features" specs
+# require 'simplecov'
 
-SimpleCov.start do
-  add_filter %r{^/spec/}
-  enable_coverage :branch
-  enable_coverage_for_eval
-  minimum_coverage_by_file line: 95, branch: 95
-end
+# SimpleCov.start do
+#   add_filter %r{^/spec/}
+#   enable_coverage :branch
+#   enable_coverage_for_eval
+#   minimum_coverage_by_file line: 95, branch: 95
+# end
