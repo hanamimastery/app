be4678029ac421a0c5033de0b5a54712c1248abb
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-11-15 12:46:53 +0100

    Make app settings a private dependency for context

--- slices/main/views/context.rb
+++ slices/main/views/context.rb
@@ -4,6 +4,7 @@ module Main
   module Views
     class Context < Hanami::View::Context
       include Deps["settings"]
+      private :settings
 
       def font_awesome_kit
         settings.font_awesome_kit
