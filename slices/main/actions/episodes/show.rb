e91e75f4f66a0f368c933b7579df8e2e1dd9e9e8
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-11-26 18:27:47 +0100

    Add index action for episodes

---
+++ slices/main/actions/episodes/show.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'byebug'
+module Main
+  module Actions
+    module Episodes
+      class Index < Main::Action
+        def handle(request, response)
+          paginate(request.params) { response.render view }
+        end
+      end
+    end
+  end
+end
