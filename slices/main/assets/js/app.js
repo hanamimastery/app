3892e1b0e8e927848c2132dc7541af9a0154b40c
Sebastian Wilgosz <sebastian@hanamimastery.com>
2023-12-02 01:20:48 +0100

    Fix htmx requests to work with sessions enabled

--- slices/main/assets/js/app.js
+++ slices/main/assets/js/app.js
@@ -1,18 +1,10 @@
 import "../css/app.css";
+console.log('LOADED MAIN APP.js 2');
 
-(() => {
-  console.log('fuuuuu')
-  document.body.addEventListener('htmx:configRequest', (event) => {
-    let csrf_param = document.querySelector('meta[name="csrf-param"]').content
-    let csrf_token = document.querySelector('meta[name="csrf-token"]').content
+document.body.addEventListener('htmx:configRequest', (event) => {
+  let csrf_param = document.querySelector('meta[name="csrf-param"]').content;
+  let csrf_token = document.querySelector('meta[name="csrf-token"]').content;
 
-    // event.detail.headers['X-CSRF-Token'] = '{{ csrf_token }}';
-    // event.detail.params['_csrf_token'] = '{{ csrf_token }}';
-    // #   <meta name="csrf-param" content="_csrf_token">
-    // #   <meta name="csrf-token" content="4a038be85b7603c406dcbfad4b9cdf91ec6ca138ed6441163a07bb0fdfbe25b5">
-    console.log('*****************')
-    console.log(csrf_param);
-    console.log(csrf_token);
-
-  })
-})
+  event.detail.headers['X-CSRF-Token'] = csrf_token;
+  event.detail.parameters['_csrf_token'] = csrf_token;
+});
