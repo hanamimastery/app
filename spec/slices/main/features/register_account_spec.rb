1f40ce2d23710dc1ca13629bba1a16a73ceb18c8
Sebastian Wilgosz <sebastian@hanamimastery.com>
2024-10-10 15:38:12 +0200

    Fix feature tests for registrations

--- spec/slices/main/features/register_account_spec.rb
+++ spec/slices/main/features/register_account_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.feature "Account registrations" do
+RSpec.feature "Account registrations", :main_db do
   scenario "Renders the form" do
     visit '/signup'
-    expect(page).to have_content('Username')
+    expect(page).to have_content('Nickname')
     expect(page).to have_content('Password')
     expect(page).to have_content('Password confirmation')
     expect(page).to have_content('I agree to the terms and conditions & privacy policy')
@@ -13,7 +13,7 @@ RSpec.feature "Account registrations" do
   scenario "Renders successful notice" do
     visit '/signup'
 
-    fill_in 'Username', with: 'jsmith'
+    fill_in 'Nickname', with: 'jsmith'
     fill_in 'Password', with: 'secret'
     fill_in 'Password confirmation', with: 'secret'
     check 'registration-tac'
@@ -25,11 +25,11 @@ RSpec.feature "Account registrations" do
   scenario "Renders error notice" do
     visit '/signup'
 
-    fill_in 'Username', with: 'jsmith'
+    fill_in 'Nickname', with: 'jsmith'
     fill_in 'Password confirmation', with: 'secret'
     check 'registration-tac'
     click_button 'Register account'
-    expect(page).to have_content('Error occured while creating the account')
+    expect(page).to have_content('There were errors with your form validation')
     expect(page).to have_content('must be filled')
   end
 end
