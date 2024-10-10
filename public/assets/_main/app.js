e7e710dd9eaa56797e27a4804aa6077e1587b8eb
Sebastian Wilgosz <sebastian@hanamimastery.com>
2024-10-10 15:37:50 +0200

    Fix assets loading

--- public/assets/_main/app.js
+++ public/assets/_main/app.js
@@ -1,2 +1,10 @@
 (() => {
+  // slices/main/assets/js/app.js
+  console.log("LOADED MAIN APP.js 2");
+  document.body.addEventListener("htmx:configRequest", (event) => {
+    let csrf_param = document.querySelector('meta[name="csrf-param"]').content;
+    let csrf_token = document.querySelector('meta[name="csrf-token"]').content;
+    event.detail.headers["X-CSRF-Token"] = csrf_token;
+    event.detail.parameters["_csrf_token"] = csrf_token;
+  });
 })();
